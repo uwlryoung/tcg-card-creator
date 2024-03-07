a8b84e8250cdc09462843f985a551b82a0dbfa66
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2024-03-07 16:31:21 -0500

    Deleted unnecsssary react code. Added the beginnings of a navbar

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h2>TCG Card Creator</h2>
+      <h4>Home</h4>
+      <h4>Create Card</h4>
+      <h4>About</h4>
+    </div>
+  );
+}
+ 
+export default Navbar;
